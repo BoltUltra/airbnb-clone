01380b48cb70417da47d427de36f21f50dd56cac
BoltUltra <mayowa.u.sunusi@gmail.com>
2022-10-27 16:08:41 +0100

    Finished Header and started with the banner

---
+++ components/Header.jsx
@@ -0,0 +1,47 @@
+import Image from "next/image";
+import {
+  MagnifyingGlassIcon,
+  GlobeAltIcon,
+  Bars3Icon,
+  UserCircleIcon,
+  UsersIcon,
+} from "@heroicons/react/24/solid";
+
+const Header = () => {
+  return (
+    <header className="grid grid-cols-3 sticky top-0 z-50 bg-white shadow-md p-5 md:px-10">
+      {/* This is the Logo */}
+      <div className="relative flex items-center h-10 cursor-pointer my-auto">
+        <Image
+          src="https://links.papareact.com/qd3"
+          width={100}
+          height={10}
+          object-fit="contain"
+          objectPosition="left"
+        />
+      </div>
+
+      {/* This is the searchbar */}
+      <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
+        <input
+          type="text"
+          placeholder="Start your search"
+          className="md:placeholder:text-sm text-sm text-gray-600 placeholder:text-xs placeholder:text-gray-400 pl-5 bg-transparent focus:outline-none flex-grow"
+        />
+        <MagnifyingGlassIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" />
+      </div>
+
+      {/* This is the menu */}
+      <div className="flex items-center space-x-4 justify-end text-gray-500">
+        <p className="hidden md:inline cursor-pointer">Become a host</p>
+        <GlobeAltIcon className="h-6 cursor-pointer" />
+        <div className="flex border-2 rounded-full p-2 space-x-2">
+          <Bars3Icon className="h-6" />
+          <UserCircleIcon className="h-6" />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Header from "../components/Header";
+import Image from "next/image";
+import { Header, Banner } from "../components";
 
 const Home: NextPage = () => {
   return (
@@ -11,7 +12,7 @@ const Home: NextPage = () => {
       </Head>
 
       <Header />
-      {/* Banner */}
+      <Banner/>
     </div>
   );
 };
